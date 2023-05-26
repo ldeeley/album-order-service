dd312d2db0c74647a08849264837ffa44a2a50c4
ldeeley <lester.deeley@yahoo.com>
2023-05-26 11:56:34 +0100

    Initial commit

---
+++ src/main/java/com/example/albumorderservice/AlbumOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.albumorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbumOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/albumorderservice/AlbumOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.albumorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
